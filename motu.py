2ef394006bb4808c7f0e9b8c5e11a7d32e7b8361
unknown <maxime.farin1@gmail.com>
2018-01-10 14:36:18 +0100

    Modification de motu.py pour faire une démo de RT si qqn visite le labo

--- motu.py
+++ motu.py
@@ -74,7 +74,7 @@ class motu:
             return (self.dataOut0.tostring(), pyaudio.paContinue) # return 0 signal in output
 
         # mise en forme des vecteurs de donnees
-        self.data4[:, :] = np.fromstring(in_data, dtype = np.int32).reshape([self.CHUNK, self.CHANNELS])# input data in_data are written in matrix data4
+        self.data4[:, :] = np.frombuffer(in_data, dtype = np.int32).reshape([self.CHUNK, self.CHANNELS])# input data in_data are written in matrix data4
         for k in range(len(self.ChannelsIn)): # size depends on the number of channels we choose
             self.data3[:, k] = self.data4[:, self.ChannelsIn[k]] # self.data3 contains the input data
 
@@ -213,29 +213,25 @@ class motu:
         '''
         Close the stream
         '''
+        self.stream.stop_stream()
         self.stream.close()
         self.p.terminate()
-        del self.p
-        del self.stream
 
 
 
 if __name__== '__main__': # mettre ceci dans un if permet de lancer le script motu.py ou de l'utiliser comme library comme import motu.py sans que cette partie soit lue
+    
     m = motu() # define a class motu named m
-    plt.cla() # clear axis
     
-    FREQ0 = [100, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000]
-    FREQ1 = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000]
-    ChannelsOut = [1]#list(range(32))
-    ChannelsOut = [c - 1 for c in ChannelsOut] # must retain 1 because sensor number = python number +1
-    ChannelsIn = [9] #capteur piezo sur la plaque
-    ChannelsIn = [c - 1 for c in ChannelsIn]
-        
+    try:
     
-    for ff in range(len(FREQ0)):
-    
-        freq0 = FREQ0[ff]
-        freq1 = FREQ1[ff]
+        ChannelsOut = list(range(10)) # Output channels
+        ChannelsOut = [c - 1 for c in ChannelsOut] # must retain 1 because sensor number = python number +1
+        ChannelsIn = [9] #capteur piezo sur la plaque
+        ChannelsIn = [c - 1 for c in ChannelsIn]
+            
+        freq0 = 3000 # Minimum frequency of the emitted chirp
+        freq1 = 8000 # Maximum frequency of the emitted chirp
         
         # emits a chirp from each ChannelsOut successively and record it on ChannelIn
         impulse = np.zeros([ceil(m.RATE*m.dureeImpulse), len(ChannelsOut)])
@@ -246,60 +242,42 @@ if __name__== '__main__': # mettre ceci dans un if permet de lancer le script mo
         
         time_impulse = np.arange(0.0, m.dureeImpulse, 1.0 / float(m.RATE))
         
-        '''
+        # Plot of the played impulse, recorded on the plate
         plt.plot(time_impulse, impulse)
         plt.xlabel("Time [s]")
         plt.ylabel("Counts")
+        plt.title("Played impulse recorded on plate piezo")
+        plt.rc('font', size = 18)
         plt.show()
-        '''
+        
 
         # use the fact that the sound path is reversible h(-t) = h(t)
         TRSignal = impulse[: : -1, :] / np.abs(impulse).max() # signal is reserved temporally and normalized to 1
         time_result = np.arange(0.0, m.RECORD_SECONDS, 1.0 / float(m.RATE)) 
         
-        
-        savefilename = "20180109_32capteurs_" + str(FREQ0[ff]) + "_" + str(FREQ1[ff]) + "Hz"
-        ChannelsPlate = [9, 10, 11, 12, 13, 14]
+        ChannelsPlate = [9]#, 10, 11, 12, 13, 14] # Piezoelectric sensor on the plate
         ChannelsPlate = [c - 1 for c in ChannelsPlate]
-
-        max_amplitude = np.zeros([len(ChannelsPlate), 1], dtype = np.float)
-        energy = np.zeros([len(ChannelsPlate), 1], dtype = np.float)
         
         for k in range(len(ChannelsPlate)):
             result = m.PlayAndRec(ChannelsPlate[k], ChannelsOut, OutFun = TRSignal)
             # reversed signal is reemitted to focus an impulse on a particular point on the plate
         
-            max_amplitude[k] = max(np.abs(result)) # save the maximum amplitude to compute the PSF
-            energy[k] = np.trapz(np.abs(result[:, 0])**2, x = time_result) # integral of the squared signal
-            '''
-            if ChannelsPlate[k] == 8:
-                plt.plot(time_result, result, 'k')
-                plt.xlabel("Time [s]")
-                plt.ylabel("Counts")
-                plt.title("Impulse focused on sensor " + str(ChannelsIn[0] + 1) + ", recorded on sensor " + str(ChannelsPlate[k] + 1))
-                plt.rc('font', size = 18)
-                plt.show()#block = False)
+            # Plot of the impulse recorded on the plate after time reversal
+            plt.plot(time_result, result, 'k')
+            plt.xlabel("Time [s]")
+            plt.ylabel("Counts")
+            plt.title("Impulse focused on plate piezo")
+            plt.rc('font', size = 18)
+            plt.show()
 
-            '''
             
-             
-        Distance_vect = [0, 21.5, 8, 18.5, 25, 9.5] #[18.5, 11, 12, 0, 7.5, 16] # distance from sensor 12
-        PSF = np.column_stack((np.array(Distance_vect).reshape(6,1), np.array(max_amplitude)))
-        PSF = PSF[PSF[:,0].argsort()]
-        
-        Distance_cm = PSF[:, 0]
-        PSF_values = 10*np.log10(PSF[:, 1] / max(PSF[:, 1]))
-        Frequency_range = [FREQ0[ff], FREQ1[ff]]
-        '''
-        filename = savefilename + "_PSF"
-        data = {'Distance_cm':Distance_cm, 'PSF':PSF_values, 'ChannelsPlate':ChannelsPlate, 'Frequency_range':Frequency_range, 'max_amplitude':max_amplitude, 'energy':energy}
-        savemat(filename, data)
-        '''
-        # Plot the Point Spread Function (PSF)
-        plt.plot(Distance_cm, PSF_values, 'k')
-        plt.xlabel("Distance [cm]")
-        plt.ylabel("PSF [dB]")
-        plt.title(savefilename)
-        plt.rc('font', size = 18)
-        plt.show()
+        # Close the stream after recording is done
+        m.stream.stop_stream()
+        m.stream.close()
+        m.p.terminate()
+    
+    except: # still close the stream if an error occurs
+        m.stream.stop_stream()
+        m.stream.close()
+        m.p.terminate()
         
